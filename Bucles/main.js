4e135cd4f0c5f6128918419ae1c3bf8851d8317b
LuisDavid2003 <l.raya.m457@edu.utc.mx>
2025-03-06 07:48:38 -0600

    Bucles

---
+++ Bucles/main.js
@@ -0,0 +1,16 @@
+for(let i=10; i<=10; i++){
+    console.log("con for" + i);
+}
+/*
+let contador=1
+while (contador<=10){
+    console.log("con while "+ contador);
+    contador++
+}
+*/
+let contador=10
+do{
+    console.log("con while "+ contador);
+    contador++
+}
+while (contador<=10);
